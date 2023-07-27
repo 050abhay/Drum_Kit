d79374e37d01bd3530dafd336e6c02a3f7b0b213
Abhay Kumar Pandey <92258727+050abhay@users.noreply.github.com>
2023-07-27 18:08:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
+
+}
+
+document.addEventListener("keydown", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var img1 = new Audio("sounds/img-1.mp3");
+      img1.play();
+      break;
+
+    case "a":
+      var img2 = new Audio("sounds/img-2.mp3");
+      img2.play();
+      break;
+
+    case "s":
+      var img3 = new Audio('sounds/img-3.mp3');
+      img3.play();
+      break;
+
+    case "d":
+      var img4 = new Audio('sounds/img-4.mp3');
+      img4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+
+    default: console.log(key);
+
+  }
+}
+
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
